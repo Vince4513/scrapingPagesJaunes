68b906e17ed0381b09ad21ba00847fee52046e75
Vincent Reau <vincent.reau1@gmail.com>
2025-03-18 11:20:20 +0100

    feat: :sparkles: Use the pipeline to clean scrape data

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ Python Framework
 - Initial release : 26 June 2008
 - Github repo : [Scrapy](https://github.com/scrapy/scrapy)
 - [Scrapy Playbook](https://scrapeops.io/python-scrapy-playbook/)
+
+## Commands
+#### Activate venv
+- poetry shell
+
+#### Activate scrapy environment
+- scrapy shell
+
+#### Create a project
+- scrapy startproject <project_name>
+
+#### Create a spider 
+- scrapy genspider <spider_name> <website_url>
+
+#### Check we are in the right folder to run our spiders
+- scrapy list
+
+#### Save data to a file
+- scrapy crawl <spider_name> -O <file_name>.csv
+- scrapy crawl <spider_name> -O <file_name>.json
--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -4,3 +4,31 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+
+class BookscraperItem(scrapy.Item):
+    # define the fields for your item here like
+    name = scrapy.Field()
+# End class BookscraperItem
+
+# If no use of pipelines, you can use functions like this 
+# price_excl_tax = scrapy.Field(serializer = serialize_price)
+def serialize_price(value):
+    return f'${str(value)}'
+# End def serialize_price
+
+class BookItem(scrapy.Item):
+    # define the fields for your item here like
+    url = scrapy.Field()
+    title = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+# End class BookItem
